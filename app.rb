bdd72212796f254edabb05fcf0472a59615db983
Mike Connolly <mjc86@hotmail.co.uk>
2021-06-15 17:52:16 +0100

    end of day 2

--- app.rb
+++ app.rb
@@ -1,35 +1,29 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
-
+​
 get '/' do
   "Hello World"
 end
-
+​
 get '/secret' do
   "Secret code! Shhh!!"
 end
-
+​
 get '/hello' do
   "Hello :)"
 end
-
+​
 get '/random-cat' do
-  @random_name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)  
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
 end
-
-get '/named-cat' do
+​
+get '/cat-form' do
+  erb :cat_form
+end
+​
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)  
-end
-
-get '/cat_name' do
-  erb :cat_name
-end
-
-post '/cat_name' do
-  p params
-  @name = params[:name]
-  erb(:cat_name)
+  erb :index
 end
